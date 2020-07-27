d8adf7a47ad9b72b424560e788c4e8b35ad847c1
lindacoco <lindacoco0204@gmail.com>
2020-07-28 07:13:47 +0900

    multiplication table, bubble sort

---
+++ algo/src/algo/practice/bubblesort.java
@@ -0,0 +1,32 @@
+package algo.practice;
+
+import java.util.Arrays;
+
+public class bubblesort {
+
+	public static void main(String[] args) {
+           int[] a= {6, 18,55,7};
+           int temp;
+           for(int i=0; i<a.length; i++) {
+        	   for(int j=0; j<a.length-i -1; j++) {
+        		   if(a[j]> a[j+1]) {
+        			   temp = a[j];
+        			   a[j] = a[j+1];
+        			   a[j+1] = temp;
+        		   }
+        	   }
+           }
+           for(int i= 0; i<a.length; i++) {
+        	   System.out.print(a[i]+" ");
+           }
+           
+       System.out.printf("\n"+"Arrays.sort이용\n");
+       Arrays.parallelSort(a);
+       for(int i= 0; i<a.length; i++) {
+    	   System.out.print(a[i]+" ");
+       }
+           
+	}
+	
+
+}
---
+++ algo/src/algo/practice/gugudan.java
@@ -0,0 +1,31 @@
+package algo.practice;
+
+public class gugudan {
+
+	public static void main(String[] args) {
+		
+		
+		for(int i=2; i<10; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.println(i+"*"+j+"= " + i*j);
+			}
+		}
+
+		System.out.println("===============반대로 9단부터");
+		
+		for(int i=9; i>0; i--) {
+			for(int j=1; j<10; j++) {
+				
+				System.out.println(i+"*"+j+"= "+i*j);
+			}
+		}
+		
+		System.out.println("특정 구구단만.. 3단!");
+		int numm =3;
+		for(int i=1; i<10; i++) {
+			System.out.println(numm+"*"+i+"= "+ numm*i);
+		}
+		
+	}
+
+}

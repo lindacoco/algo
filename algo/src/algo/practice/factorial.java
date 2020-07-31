43a54545676738d8f2b098bcea933f24ae0d9252
lindacoco <lindacoco0204@gmail.com>
2020-07-31 13:51:23 +0900

    factorial

---
+++ algo/src/algo/practice/factorial.java
@@ -0,0 +1,20 @@
+package algo.practice;
+
+public class factorial {
+	
+	public static void main(String[] args) {
+	    int num = 5;
+		System.out.println(num+"! = "); 
+		
+		
+		int result =1;
+		for(int i= num; i>0; i--) {
+			result *=i;
+			System.out.print(i);
+			System.out.print(i==1?"=":"*");
+		  }
+		  System.out.println(result);
+	
+	   }
+	
+}
